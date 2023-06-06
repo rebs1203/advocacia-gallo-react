0c61fd8fef3571113458a0b27a9a1247439e1e9f
rebecavg <rebecavgallo@hotmail.com>
2023-06-06 19:46:44 -0400

    fixed some styles

--- src/components /AreasCom.jsx
+++ src/components /AreasCom.jsx
@@ -4,34 +4,34 @@ import '../styles/areas-com.css'
 const AreasCom = () => {
     return (
         <div>
-            <h2>Áreas de Atuação</h2>
+            <h2 className='areas-title'>Áreas de Atuação</h2>
             <div>
-                <h3>Cívil</h3>
-                <p>A área de advocacia cível abrange uma ampla gama de questões relacionadas ao direito civil. Os profissionais que atuam nessa área estão constantemente empenhados em encontrar soluções que tragam benefícios para as empresas. Isso pode envolver tanto ações preventivas, como a revisão de contratos, quanto abordagens proativas, como a recuperação de crédito. O objetivo dos advogados especializados em advocacia cível é garantir a proteção dos direitos e interesses das empresas, fornecendo um suporte jurídico completo e eficaz.</p>
+                <h3 className='elem-title'>Cívil</h3>
+                <p className='elem-text'>A área de advocacia cível abrange uma ampla gama de questões relacionadas ao direito civil. Os profissionais que atuam nessa área estão constantemente empenhados em encontrar soluções que tragam benefícios para as empresas. Isso pode envolver tanto ações preventivas, como a revisão de contratos, quanto abordagens proativas, como a recuperação de crédito. O objetivo dos advogados especializados em advocacia cível é garantir a proteção dos direitos e interesses das empresas, fornecendo um suporte jurídico completo e eficaz.</p>
             </div>
             <div>
-                <h3>Imobiliária</h3>
-                <p>A advocacia imobiliária é uma área de atuação que abrange uma ampla variedade de questões legais relacionadas ao mercado imobiliário. Os advogados especializados nessa área estão empenhados em encontrar soluções que beneficiem seus clientes em assuntos como transações imobiliárias, contratos de locação, regularização de propriedades e disputas envolvendo imóveis.</p>
+                <h3 className='elem-title'>Imobiliária</h3>
+                <p className='elem-text'>A advocacia imobiliária é uma área de atuação que abrange uma ampla variedade de questões legais relacionadas ao mercado imobiliário. Os advogados especializados nessa área estão empenhados em encontrar soluções que beneficiem seus clientes em assuntos como transações imobiliárias, contratos de locação, regularização de propriedades e disputas envolvendo imóveis.</p>
             </div>
             <div>
-                <h3>Empresarial</h3>
-                <p>A advocacia empresarial é uma área de atuação que abrange uma ampla gama de questões jurídicas relacionadas às empresas. Os advogados especializados nesse ramo estão constantemente empenhados em encontrar soluções que beneficiem seus clientes, buscando oferecer suporte jurídico completo e eficaz. Isso pode envolver desde ações preventivas, como a elaboração e revisão de contratos, até estratégias proativas, como a resolução de disputas e a assessoria em questões regulatórias. O objetivo dos advogados de advocacia empresarial é garantir a proteção dos direitos e interesses das empresas, ajudando-as a enfrentar os desafios legais de forma segura e assertiva.</p>
+                <h3 className='elem-title'>Empresarial</h3>
+                <p className='elem-text'>A advocacia empresarial é uma área de atuação que abrange uma ampla gama de questões jurídicas relacionadas às empresas. Os advogados especializados nesse ramo estão constantemente empenhados em encontrar soluções que beneficiem seus clientes, buscando oferecer suporte jurídico completo e eficaz. Isso pode envolver desde ações preventivas, como a elaboração e revisão de contratos, até estratégias proativas, como a resolução de disputas e a assessoria em questões regulatórias. O objetivo dos advogados de advocacia empresarial é garantir a proteção dos direitos e interesses das empresas, ajudando-as a enfrentar os desafios legais de forma segura e assertiva.</p>
             </div>
             <div>
-                <h3>Inventário</h3>
-                <p>A advocacia de inventário é uma área de atuação jurídica que abrange todas as questões relacionadas ao processo de inventário e partilha de bens. Os advogados especializados nessa área estão comprometidos em oferecer suporte jurídico completo e eficaz aos seus clientes durante esse procedimento.<br/>O objetivo dos advogados especializados em advocacia de inventário é garantir uma administração adequada e eficiente dos bens deixados pelo falecido, bem como assegurar que a divisão seja realizada de acordo com a lei e os desejos expressos pelo falecido em testamento, se houver.</p>
+                <h3 className='elem-title'>Inventário</h3>
+                <p className='elem-text'>A advocacia de inventário é uma área de atuação jurídica que abrange todas as questões relacionadas ao processo de inventário e partilha de bens. Os advogados especializados nessa área estão comprometidos em oferecer suporte jurídico completo e eficaz aos seus clientes durante esse procedimento.<br/>O objetivo dos advogados especializados em advocacia de inventário é garantir uma administração adequada e eficiente dos bens deixados pelo falecido, bem como assegurar que a divisão seja realizada de acordo com a lei e os desejos expressos pelo falecido em testamento, se houver.</p>
             </div>
             <div>
-                <h3>Familía</h3>
-                <p>O objetivo dos advogados especializados em advocacia familiar é proteger os direitos e interesses de seus clientes, buscando soluções equilibradas e justas para as questões familiares em disputa. Eles oferecem orientação especializada, ajudando os clientes a compreenderem seus direitos e deveres legais, além de auxiliá-los na tomada de decisões importantes que afetam o futuro de suas famílias.</p>
+                <h3 className='elem-title'>Familía</h3>
+                <p className='elem-text'>O objetivo dos advogados especializados em advocacia familiar é proteger os direitos e interesses de seus clientes, buscando soluções equilibradas e justas para as questões familiares em disputa. Eles oferecem orientação especializada, ajudando os clientes a compreenderem seus direitos e deveres legais, além de auxiliá-los na tomada de decisões importantes que afetam o futuro de suas famílias.</p>
             </div>
             <div>
-                <h3>Contrato</h3>
-                <p>A advocacia de contratos é uma área de atuação jurídica que abrange todas as questões relacionadas à elaboração, revisão e interpretação de contratos. Os advogados especializados nessa área estão comprometidos em oferecer suporte jurídico completo e eficaz aos seus clientes em relação a contratos comerciais, contratos de prestação de serviços, contratos de compra e venda, entre outros.<br/>Além disso, os advogados especializados em advocacia de contratos estão aptos a analisar contratos existentes, identificar possíveis lacunas ou cláusulas problemáticas e propor soluções adequadas para evitar litígios futuros. Eles também podem auxiliar na negociação de termos contratuais, assegurando que seus clientes obtenham os melhores acordos possíveis.</p>
+                <h3 className='elem-title'>Contrato</h3>
+                <p className='elem-text'>A advocacia de contratos é uma área de atuação jurídica que abrange todas as questões relacionadas à elaboração, revisão e interpretação de contratos. Os advogados especializados nessa área estão comprometidos em oferecer suporte jurídico completo e eficaz aos seus clientes em relação a contratos comerciais, contratos de prestação de serviços, contratos de compra e venda, entre outros.<br/>Além disso, os advogados especializados em advocacia de contratos estão aptos a analisar contratos existentes, identificar possíveis lacunas ou cláusulas problemáticas e propor soluções adequadas para evitar litígios futuros. Eles também podem auxiliar na negociação de termos contratuais, assegurando que seus clientes obtenham os melhores acordos possíveis.</p>
             </div>
             <div>
-                <h3>Direito de Imigração</h3>
-                <p>A advocacia de direito de imigração é uma área especializada que abrange todas as questões legais relacionadas a imigração e status migratório. Os advogados especializados nessa área estão comprometidos em oferecer suporte jurídico completo e eficaz aos seus clientes em questões como vistos, residência permanente, cidadania, deportação e outras questões relacionadas ao direito de imigração.<br/>Os advogados especializados em direito de imigração podem ajudar na preparação e apresentação de petições, responder a notificações e representar seus clientes em processos administrativos ou judiciais relacionados à imigração. Seu objetivo é proteger os direitos e interesses de seus clientes, buscando soluções adequadas e legais para as questões migratórias.</p>
+                <h3 className='elem-title'>Direito de Imigração</h3>
+                <p className='elem-text'>A advocacia de direito de imigração é uma área especializada que abrange todas as questões legais relacionadas a imigração e status migratório. Os advogados especializados nessa área estão comprometidos em oferecer suporte jurídico completo e eficaz aos seus clientes em questões como vistos, residência permanente, cidadania, deportação e outras questões relacionadas ao direito de imigração.<br/>Os advogados especializados em direito de imigração podem ajudar na preparação e apresentação de petições, responder a notificações e representar seus clientes em processos administrativos ou judiciais relacionados à imigração. Seu objetivo é proteger os direitos e interesses de seus clientes, buscando soluções adequadas e legais para as questões migratórias.</p>
             </div>
         </div>
     )
